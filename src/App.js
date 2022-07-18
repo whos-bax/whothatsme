50e3063b5ba742a581bca5c511b3847261b16924
who <loot04@naver.com>
2022-07-18 21:44:02 +0900

    studio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
+import Home from './pages/Home';  
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Home() {
     return (
-    < h1 > 반갑습니다</h1 >
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
 
