98de56a3eac79dd35ce8193dc00536e74aefd21b
whos-bax <loot04@naver.com>
2022-07-19 21:57:22 +0900

    home 소개 글

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Navbar, Container, Nav, Image, Alert } from "react-bootstrap";
+import { Navbar, Container, Nav, Image, Alert, Button } from "react-bootstrap";
 
 function Home() {
   const homeStyle = {
@@ -8,42 +8,38 @@ function Home() {
     justifyContent: "center",
   };
 
-  const homeContents = {
-    
-  };
+  const homeContents = {};
 
   const visibleAnima = useRef();
+  const useBlur = useRef();
 
   useEffect(() => {
     setTimeout(() => {
       visibleAnima.current.animate({ opacity: 1 }, 400);
       // visibleAnima.current.style.opacity = 1;
     }, 1500);
+    setTimeout(() => {
+      useBlur.current.style.backdropFilter = "blur(5px) opacity(1)";
+      useBlur.current.style.opacity = 1;
+    }, 1000);
   });
 
   return (
     <Container
       fluid
-      className="home backgroundStudio d-flex"
+      className="d-flex backgroundStudio"
       style={homeStyle}
       ref={visibleAnima}
     >
-      <div className="d-flex homeContent" style={homeContents}>
-        꿈이 많은 어른 아이
+      <div className="d-flex m-auto homeContent" ref={useBlur}>
+        <h1>어제보다 오늘 더</h1>
+        <p>
+          안녕하세요 꿈이 많은 어른 아이 박상호입니다. <br />늘 새로운 도전을
+          추구하며 보다 더 세상에 영향을 주는 사람을 꿈꾸고 있습니다.
+        </p>
+      <Button variant="outline-light" className="moreBtn">더 알아보기</Button>
       </div>
     </Container>
-    // <Navbar bg="dark" variant="dark">
-    //     <Image src="img/studio.jpg" style={{width: '18em'}} alt="logo"/>
-    // {/* <img src="img/studio.jpg" className="App" alt="logo" /> */}
-    //     <Container>
-    //         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    //         <Nav className="me-auto">
-    //             <Nav.Link href="#home">Home</Nav.Link>
-    //             <Nav.Link href="#features">Features</Nav.Link>
-    //             <Nav.Link href="#pricing">Pricing</Nav.Link>
-    //         </Nav>
-    //     </Container>
-    // </Navbar>
   );
 }
 
