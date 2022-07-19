81ee91c690703901a8e3a1b716dfbb7d90ccc802
whos-bax <loot04@naver.com>
2022-07-20 01:50:42 +0900

    home에서 app 으로 navbar와 footer로 사용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import logo from "./logo.svg";
+import { useRef, useState } from "react";
 import "./App.css";
-import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ProgressBar } from "react-bootstrap";
+import Home from "./pages/Home";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const blackScreenUp = useRef();
+  const blackScreenDown = useRef();
+  const startDiv = useRef();
+  const startBtn = useRef();
+
+  const [start, setStart] = useState(false);
+
+  const blackScreen = {
+    position: "absolute",
+    width: "100%",
+    backgroundColor: "#141414",
+    height: "50vh",
+    transition: "all 2s ease-in-out",
+  };
+
+  function clickStartBtn() {
+    startBtn.current.style.width = 0;
+    startBtn.current.style.height = 0;
+    setTimeout(() => {
+      startBtn.current.style.width = "100%";
+    }, 500);
+    setTimeout(() => {
+      startBtn.current.style.opacity = 0;
+      blackScreenUp.current.style.height = "0";
+      blackScreenDown.current.style.height = "0";
+    }, 1800);
+    setTimeout(() => {
+      blackScreenUp.current.style.height = "4rem";
+      blackScreenDown.current.style.height = "4rem";
+      startBtn.current.style.display = "none";
+      startDiv.current.style.display = "none";
+      setStart(true);
+    }, 3800);
+  }
+
   return (
     <div className="App">
-      <Home />
-      안녕하세요
+      <div style={blackScreen} id="blackScreenUp" ref={blackScreenUp}></div>
+      <div
+        style={{ width: "100%", height: "100%", display: "flex" }}
+        ref={startDiv}
+      >
+        <Button
+          className="startBtn"
+          variant="outline-light"
+          ref={startBtn}
+          onClick={clickStartBtn}
+        />
+      </div>
+      {/* <Home props={start} /> */}
+
+      <div style={blackScreen} id="blackScreenDown" ref={blackScreenDown}></div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,79 +1,45 @@
 import React, { useRef } from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Navbar } from "react-bootstrap";
 import "../css/Home.css";
 
-function Home() {
+function Home(start) {
+    console.log(start)
   const homeStyle = {
     width: "100%",
     height: "100%",
     justifyContent: "center",
-    // overflow: "hidden"
   };
 
-  const blackScreen = {
-    position: "absolute",
-    width: "100%",
-    backgroundColor: "#191919",
-    height: "50vh",
-    transition: "all 2s ease-in-out",
-  };
-
-  const blackScreenUp = useRef();
-  const blackScreenDown = useRef();
   const useBlur = useRef();
-  const startDiv = useRef();
-  const startBtn = useRef();
 
-  function letsStart() {
-    startBtn.current.style.width = 0;
-    startBtn.current.style.height = 0;
-    setTimeout(() => {
-      startBtn.current.style.width = "100%";
-    }, 500);
-    setTimeout(() => {
-      startBtn.current.style.opacity = 0;
-      blackScreenUp.current.style.height = "0";
-      blackScreenDown.current.style.height = "0";
-    }, 1800);
-    setTimeout(() => {
-      blackScreenUp.current.style.height = "4rem";
-      blackScreenDown.current.style.height = "4rem";
-      startBtn.current.style.display = "none";
-      startDiv.current.style.display = "none";
-      useBlur.current.style.backdropFilter = "blur(10px)";
-      useBlur.current.style.opacity = 1;
-      useBlur.current.style.gap = "2rem";
-    }, 3800);
+  function clickMoreBtn() {
+    useBlur.current.style.backdropFilter = "none";
+    useBlur.current.style.display = "none";
+  }
+  if (start) {
+    useBlur.current.style.backdropFilter = "blur(10px)";
+    useBlur.current.style.opacity = 1;
+    useBlur.current.style.gap = "2rem";
   }
 
-  console.log()
+  console.log();
 
   return (
     <Container fluid className="backgroundStudio" style={homeStyle}>
-      <div style={blackScreen} id="blackScreenUp" ref={blackScreenUp}></div>
-      <div
-        style={{ width: "100%", height: "100%", display: "flex" }}
-        ref={startDiv}
-      >
-        <Button
-          className="startBtn"
-          variant="outline-light"
-          ref={startBtn}
-          onClick={letsStart}
-        />
-      </div>
       <div className="d-flex m-auto homeContent" ref={useBlur}>
         <h1>어제보다 오늘 더</h1>
         <p>
           반갑습니다. 꿈이 많은 어른 아이, 박상호입니다. <br />늘 새로운 도전을
           추구하며 보다 더 세상에 영향을 주는 사람을 꿈꾸고 있습니다.
         </p>
-        <Button variant="outline-light" className="moreBtn">
+        <Button
+          variant="outline-light"
+          className="moreBtn"
+          onClick={clickMoreBtn}
+        >
           더 알아보기
         </Button>
       </div>
-
-      <div style={blackScreen} id="blackScreenDown" ref={blackScreenDown}></div>
     </Container>
   );
 }
