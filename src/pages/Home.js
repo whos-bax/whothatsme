4590d337bcb95962d91630cf6177bf77059c41ba
whos-bax <loot04@naver.com>
2022-07-19 22:48:23 +0900

    home 영화같은 효과

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,37 +8,54 @@ function Home() {
     justifyContent: "center",
   };
 
-  const homeContents = {};
+  const blackScreen = {
+    position: "absolute",
+    width: "100vw",
+    backgroundColor: "black",
+    height: "50vh",
+    transition: "all 3s ease-out",
+  };
+
+  const blackScreenUp = useRef();
+  const blackScreenDown = useRef();
 
   const visibleAnima = useRef();
   const useBlur = useRef();
 
   useEffect(() => {
     setTimeout(() => {
-      visibleAnima.current.animate({ opacity: 1 }, 400);
-      // visibleAnima.current.style.opacity = 1;
-    }, 1500);
-    setTimeout(() => {
-      useBlur.current.style.backdropFilter = "blur(5px) opacity(1)";
-      useBlur.current.style.opacity = 1;
+      blackScreenUp.current.style.height = "4rem";
+      blackScreenDown.current.style.height = "4rem";
+      useBlur.current.style.display = "flex";
     }, 1000);
+
+    setTimeout(() => {
+      useBlur.current.style.backdropFilter = "blur(5px)";
+      useBlur.current.style.opacity = 1;
+    }, 4000);
   });
 
   return (
     <Container
       fluid
-      className="d-flex backgroundStudio"
+      className="backgroundStudio"
       style={homeStyle}
       ref={visibleAnima}
     >
+      <div style={blackScreen} id="blackScreenUp" ref={blackScreenUp}></div>
+
       <div className="d-flex m-auto homeContent" ref={useBlur}>
         <h1>어제보다 오늘 더</h1>
         <p>
           안녕하세요 꿈이 많은 어른 아이 박상호입니다. <br />늘 새로운 도전을
           추구하며 보다 더 세상에 영향을 주는 사람을 꿈꾸고 있습니다.
         </p>
-      <Button variant="outline-light" className="moreBtn">더 알아보기</Button>
+        <Button variant="outline-light" className="moreBtn">
+          더 알아보기
+        </Button>
       </div>
+
+      <div style={blackScreen} id="blackScreenDown" ref={blackScreenDown}></div>
     </Container>
   );
 }
