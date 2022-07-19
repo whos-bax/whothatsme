b3457b9919c9a95d91bde05a392ffe3dff481322
whos-bax <loot04@naver.com>
2022-07-19 16:42:46 +0900

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import { Navbar, Container, Nav, Image } from 'react-bootstrap';
+import React, { useEffect, useRef, useState } from "react";
+import { Navbar, Container, Nav, Image, Alert } from "react-bootstrap";
 
 function Home() {
-    return (
-        <Navbar bg="dark" variant="dark">
-            <Image src="img/studio.jpg" style={{width: '18em'}} alt="logo"/>
-        {/* <img src="img/studio.jpg" className="App" alt="logo" /> */}
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    )
+  const homeStyle = {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+  };
+
+  const homeContents = {
+    
+  };
+
+  const visibleAnima = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      visibleAnima.current.animate({ opacity: 1 }, 400);
+      // visibleAnima.current.style.opacity = 1;
+    }, 1500);
+  });
+
+  return (
+    <Container
+      fluid
+      className="home backgroundStudio d-flex"
+      style={homeStyle}
+      ref={visibleAnima}
+    >
+      <div className="d-flex homeContent" style={homeContents}>
+        꿈이 많은 어른 아이
+      </div>
+    </Container>
+    // <Navbar bg="dark" variant="dark">
+    //     <Image src="img/studio.jpg" style={{width: '18em'}} alt="logo"/>
+    // {/* <img src="img/studio.jpg" className="App" alt="logo" /> */}
+    //     <Container>
+    //         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    //         <Nav className="me-auto">
+    //             <Nav.Link href="#home">Home</Nav.Link>
+    //             <Nav.Link href="#features">Features</Nav.Link>
+    //             <Nav.Link href="#pricing">Pricing</Nav.Link>
+    //         </Nav>
+    //     </Container>
+    // </Navbar>
+  );
 }
 
 export default Home;
