528f9cc22dfb6983ba0d53acdea825f3b5f7c90c
whos-bax <loot04@naver.com>
2022-07-20 01:17:57 +0900

    home 완성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  Navbar,
-  Container,
-  Nav,
-  Image,
-  Alert,
-  Button,
-  Spinner,
-  ProgressBar,
-} from "react-bootstrap";
+import React, { useRef } from "react";
+import { Container, Button } from "react-bootstrap";
 import "../css/Home.css";
 
 function Home() {
@@ -28,65 +19,44 @@ function Home() {
 
   const blackScreenUp = useRef();
   const blackScreenDown = useRef();
-
-  const visibleAnima = useRef();
   const useBlur = useRef();
-
+  const startDiv = useRef();
   const startBtn = useRef();
-
-  const [screen, setScreen] = useState(false);
 
   function letsStart() {
     startBtn.current.style.width = 0;
     startBtn.current.style.height = 0;
-    setScreen(true);
-    console.log(screen);
     setTimeout(() => {
-      console.log("@");
       startBtn.current.style.width = "100%";
-      movieStart();
     }, 500);
-  }
-
-  function movieStart() {
-    console.log("@?", screen);
-    screen
-      ? setTimeout(() => {
-          blackScreenUp.current.style.height = "4rem";
-          blackScreenDown.current.style.height = "4rem";
-        }, 500)
-      : console.log("wrong");
-  }
-
-  useEffect(() => {
     setTimeout(() => {
-      // blackScreenUp.current.style.height = "4rem";
-      //   useBlur.current.style.display = "flex";
-    }, 1000);
-
+      startBtn.current.style.opacity = 0;
+      blackScreenUp.current.style.height = "0";
+      blackScreenDown.current.style.height = "0";
+    }, 1800);
     setTimeout(() => {
-      //   useBlur.current.style.backdropFilter = "blur(5px)";
-      //   useBlur.current.style.opacity = 1;
-      //   useBlur.current.style.gap = "2.5rem";
-    }, 3000);
-  });
+      blackScreenUp.current.style.height = "4rem";
+      blackScreenDown.current.style.height = "4rem";
+      startBtn.current.style.display = "none";
+      startDiv.current.style.display = "none";
+      useBlur.current.style.backdropFilter = "blur(10px)";
+      useBlur.current.style.opacity = 1;
+      useBlur.current.style.gap = "2rem";
+    }, 3800);
+  }
 
   return (
-    <Container
-      fluid
-      className="backgroundStudio"
-      style={homeStyle}
-      ref={visibleAnima}
-    >
-      {/* <div style={blackScreen} id="blackScreenUp" ref={blackScreenUp}></div> */}
-      <div style={{ width: "100%", height: "100%", display: "flex" }}>
+    <Container fluid className="backgroundStudio" style={homeStyle}>
+      <div style={blackScreen} id="blackScreenUp" ref={blackScreenUp}></div>
+      <div
+        style={{ width: "100%", height: "100%", display: "flex" }}
+        ref={startDiv}
+      >
         <Button
           className="startBtn"
           variant="outline-light"
           ref={startBtn}
-          //   onClick={() => {
-          //     letsStart();
-          //   }}
+          onClick={letsStart}
         />
       </div>
       <div className="d-flex m-auto homeContent" ref={useBlur}>
