1843b6701f98afb927ec3f098f59e532277539fa
whos-bax <loot04@naver.com>
2022-07-22 22:32:03 +0900

    tooltip

--- src/components/Introduce02skills.js
+++ src/components/Introduce02skills.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
-import { Button, Col, Container, ProgressBar, Row } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Overlay,
+  ProgressBar,
+  Row,
+  Tooltip,
+} from "react-bootstrap";
 import * as Icon from "../utils/Icon";
 
 function Introduce02skills() {
   const [show, setShow] = useState(false);
+  const [help, setHelp] = useState(false);
+
+  const target = useRef(null);
 
   const languages = Icon.proficient;
 
@@ -60,7 +71,28 @@ function Introduce02skills() {
         <Button id="svgBtn" onClick={() => setShow(!show)}>
           {!show ? <p>숙련도 보기</p> : <p>숙련도 닫기</p>}
         </Button>
-        <Button id="svgHelpBtn">?</Button>
+        <Button id="svgHelpBtn" onClick={() => setHelp(!help)}>
+          ?
+        </Button>
+        {help && (
+          <div id="help-tooltip">
+            <p>
+              <ProgressBar
+                animated
+                variant={"success"}
+                now={100}
+                label="능숙"
+              />
+              <ProgressBar
+                animated
+                variant={"warning"}
+                now={100}
+                label="보통"
+              />
+              <ProgressBar animated variant={"danger"} now={100} label="미흡" />
+            </p>
+          </div>
+        )}
       </Col>
     </Container>
   );
