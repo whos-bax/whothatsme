5a843f3a27f455afb50cf0e8dfdb1728beb3bead
whos-bax <loot04@naver.com>
2022-07-25 14:19:43 +0900

    app.js ref 없이 css 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Titlebar from "./components/Titlebar";
@@ -9,9 +9,6 @@ import Introduce from "./pages/Introduce";
 
 function App() {
   const contents = ["introduce", "project", "contact"];
-
-  const startDiv = useRef();
-  const startBtn = useRef();
 
   const [clickStart, setClickStart] = useState(false);
   const [afterStart, setAfterStart] = useState(false);
@@ -21,29 +18,7 @@ function App() {
   // start 버튼을 눌렀을 때
   useEffect(() => {
     if (clickStart) {
-    //   startBtn.current.style.width = 0;  
-    // startBtn.current.style.height = 0;
-    // blackScreenUp.current.style.transition = "all 1s ease-in-out";
-    // blackScreenDown.current.style.transition = "all 1s ease-in-out";
-    setWelcomeDel(false);
-      document.getElementsByClassName("blackScreen")[0].style.trasition =
-        "all 1s ease-in-out";
-
-      // 영역 나누기
-      setTimeout(() => {
-        document.getElementById("startBtn").style.width = "100%";
-      }, 700);
-
-      // setTimeout(() => {
-      // document.getElementById("startBtn").style.opacity = "0";
-      //   console.log(document.getElementsByClassName("blackScreen")[0].style);
-      document.getElementsByClassName("blackScreen")[0].style.height =
-        "0 !important";
-      //   startBtn.current.style.opacity = 0;
-      // blackScreenUp.current.style.height = "0";
-      // blackScreenDown.current.style.height = "0";
-      //   setAfterStart(true);
-      // }, 2500);
+      setWelcomeDel(false);
     }
   }, [clickStart]);
 
@@ -61,20 +36,20 @@ function App() {
 
   return (
     <Container fluid className="App">
-      <div className="blackScreen" id="blackScreenUp">
+      <div
+        className={"blackScreen" + `${clickStart ? " screenOut" : ""}`}
+        id="blackScreenUp"
+      >
         {blurQuit ? <Titlebar start={afterStart} contents={contents} /> : null}
       </div>
       {!afterStart ? (
         <div
           style={{ width: "100%", height: "100%", display: "flex" }}
-          ref={startDiv}
         >
           <Button
-            id="startBtn"
+            className={"startBtn" + `${clickStart ? " startBtnOut" : ""}`}
             variant="outline-light"
-            ref={startBtn}
             onClick={() => setClickStart(true)}
-            style={clickStart ? { width: 0, height: 0 } : null}
           >
             {welcomeDel ? <p>WELCOME</p> : null}
           </Button>
@@ -90,7 +65,10 @@ function App() {
         </>
       )}
 
-      <div className="blackScreen" id="blackScreenDown">
+      <div
+        className={"blackScreen" + `${clickStart ? " screenOut" : ""}`}
+        id="blackScreenDown"
+      >
         {blurQuit ? <Footer start={afterStart} /> : null}
       </div>
     </Container>
