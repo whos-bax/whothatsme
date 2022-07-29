601a123f5e17a3177ecca8bfa1643ad66fdc2769
whos-bax <loot04@naver.com>
2022-07-29 11:08:51 +0900

    project04

--- src/components/Project04.js
+++ src/components/Project04.js
@@ -15,11 +15,7 @@ function Project03() {
         <Row id="projectBody">
           <Col md={6}>
             <Image
-              src={process.env.PUBLIC_URL + "/images/project/project03.png"}
-              id="projectImg"
-            />
-            <Image
-              src={process.env.PUBLIC_URL + "/images/project/project03-1.png"}
+              src={process.env.PUBLIC_URL + "/images/project/project04.png"}
               id="projectImg"
             />
           </Col>
@@ -28,85 +24,64 @@ function Project03() {
               <ListGroup variant="flush" style={{ padding: 0 }}>
                 <ListGroup.Item>
                   <p id="impactText">
-                  노마드코더를 참고하여, Vanilla JS와 HTML, CSS 만으로 Momentum 만들기
+                    노마드코더를 참고하여, Vanilla JS와 HTML, CSS 만으로
+                    Momentum 만들기
                   </p>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <p>
-                  <b>팀장</b> 개인 (총 1명)
+                    <b>팀원</b> : 개인 (총 1명)
                   </p>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <p>
-                    <b>주요기능</b> : 사용자 편의를 위한 UI, 반응형 화면, 시/군/구에 따른 위치값 설정, 현재 온도와 이전 온도 값을 받고 미래 값 예측
-                  </p>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <p>
-                    <b>Frontend</b> : React, JavaScript, axios, React-Bootstrap, React-router-dom
-                  </p>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <p>
-                    <b>Backend</b> : Node.js, express
-                  </p>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <p>
-                    <b>DataProcessing</b> : Python, TensorFlow, NumPy
-                  </p>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <p>
-                    <b>Database</b> : PostgreSQL
+                    <b>주요기능</b> : 현재시간 알림, 현재위치에 따른 온도 값
+                    알기, 이름 입력 및 저장하기, 초기화, 배경화면 새로고침에
+                    따른 변화{" "}
                   </p>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <p id="linkText">
                     <a
-                      href="https://compmath.korea.ac.kr/gitlab/students/eue"
+                      href="https://github.com/whos-bax/whos-momentum"
                       target={"_blank"}
                     >
-                      Gitlab
+                      Github
                     </a>
                   </p>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <p id="linkText">
                     <a
-                      href="https://www.notion.so/dorakang612/EUE-047f1812c6c24096a528dfd3330c227d "
+                      href="https://whos-bax.github.io/whos-momentum/"
                       target={"_blank"}
                     >
-                      Notion
+                      Page
                     </a>
                   </p>
                 </ListGroup.Item>
               </ListGroup>
             </Row>
             <Row id="projectDropdown">
-            <Dropdown drop='up'>
-                <Dropdown.Toggle variant="success">
-                  배경
-                </Dropdown.Toggle>
+              <Dropdown drop="up">
+                <Dropdown.Toggle variant="success">배경</Dropdown.Toggle>
 
-                <Dropdown.Menu style={{border: "3px #171717 solid"}}>
+                <Dropdown.Menu style={{ border: "3px #171717 solid" }}>
                   <Dropdown.ItemText>
                     <p>
-                    21년 여름 방학에 진행 했던 프로젝트로, 온실가스의 배출이 많아지면서 지구온난화가 급격해지는 것을 방지하고자 <b>효율적인 에너지 사용</b>을 위한 웹 페이지를 제작하였습니다. <b>온도 유지를 위한 전력 소모가 더 큰 점을 고려하여, 온도 조절을 효율적으로 하는 방향으로 진행</b>하려 했습니다. 데이터를 수집하고, 온도를 예측하여 사용자에게 온도 조절 장치 사용을 제안하는 방향으로 진행했습니다.
+                    JavaScript에 대한 공부 없이 React로 시작한 입장에서 <b>기본을 더 공부해야겠다</b> 하는 생각이 들었습니다. 여름 프로젝트의 팀장의 조언으로 JavaScript를 더 공부해보자는 생각으로 시작했습니다.
                     </p>
                   </Dropdown.ItemText>
                 </Dropdown.Menu>
               </Dropdown>
 
-              <Dropdown drop='down'>
-                <Dropdown.Toggle variant="warning">
-                  의의
-                </Dropdown.Toggle>
+              <Dropdown drop="down">
+                <Dropdown.Toggle variant="warning">의의</Dropdown.Toggle>
 
-                <Dropdown.Menu style={{border: "3px #171717 solid"}}>
+                <Dropdown.Menu style={{ border: "3px #171717 solid" }}>
                   <Dropdown.ItemText>
                     <p>
-                    지난 프로젝트와 다르게 3명의 팀원이 협력하고 의지하여 하나의 결과물을 만들어낼 수 있었습니다. Frontend, Backend, DataProcessing, 세 부분을 각각 담당하여 모든 것이 어우러지고 조화되도록 하는 것이 주된 목적이었습니다. 거의 매일 같이 회의하고 이야기를 나누었고, <b>Frontend에 전반적인 부분을 담당</b>하면서 Chart.js, SweetAlert2 등 다양한 라이브러리를 사용해보고 디자인적으로, 기능적으로 더 잘 어울리는 부분을 고민하게 되는 소중한 시간이었습니다. 또 KakaoAPI를 이용해 보면서 회원가입과 로그인에 직접적으로 관여해보는 시간도 갖을 수 있었습니다.
+                    HTML과 Vanilla JS, CSS만을 이용해서 만들어보면서 <b>왜 React를 사용하고 Bootstrap을 사용하려 하는지에 대한 이유</b>를 이해할 수 있었습니다. React는 컴포넌트를 나누어서 사용해서 각 부분 별로 작업이 가능하기에 이 프로젝트와 비교하여 장점을 극명히 느낄 수 있었습니다. 기본이 되는 자바스크립트를 더 알고 이해하는 시간을 가질 수 있었습니다.
                     </p>
                   </Dropdown.ItemText>
                 </Dropdown.Menu>
