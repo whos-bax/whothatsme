1465e08abd23e146a941064d976bc48eda9442f2
whos-bax <loot04@naver.com>
2022-07-31 04:20:57 +0900

    시퀄프로

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-
+// const mysql = require("mysql");
+const os = require("os");
 const PORT = process.env.port || 8000;
 
-app.get('/', (req, res) => {
-    console.log("first")
-})
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "mariadb",
+  database: "test",
+});
+app.get("/", (req, res) => {
+  console.log("first");
+});
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
