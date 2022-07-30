1c5ca82b8ee88992430146098c43bb037b7a8b27
whos-bax <loot04@naver.com>
2022-07-30 23:07:48 +0900

    server 삭제

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-
-app.listen(8080, function() {
-    console.log('listening on 8080')
-})
-
-// app.get("/", function)
