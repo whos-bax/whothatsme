76c76aed9d2e232656fb9f1093ce8aa0381b1c60
whos-bax <loot04@naver.com>
2022-07-30 22:14:53 +0900

    express nodemon 설치

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(8080, function() {
+    console.log('listening on 8080')
+})
+
+// app.get("/", function)
